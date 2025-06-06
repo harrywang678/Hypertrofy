99e20a60b3aa9afab19b672d4153930db7d389ed
Harry Wang <harrywang678@gmail.com>
2025-06-06 10:09:48 -0400

    get for workouts is working

--- src/app/api/workouts/route.ts
+++ src/app/api/workouts/route.ts
@@ -1,28 +1,50 @@
-import { createWorkout } from '@/data/workouts';
-import { NextResponse, NextRequest } from 'next/server';
+import {createWorkout, getWorkoutById} from "@/data/workouts";
+import {NextResponse, NextRequest} from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const { userId, name, notes } = await req.json();
+    const {userId, name, notes} = await req.json();
 
     if (!userId || !name) {
       return NextResponse.json(
-        { error: 'userId and name are required' },
-        { status: 400 }
+        {error: "userId and name are required"},
+        {status: 400}
       );
     }
 
     const newWorkout = await createWorkout(userId, name, notes);
 
     return NextResponse.json(
-      { success: true, workout: newWorkout },
-      { status: 201 }
+      {success: true, workout: newWorkout},
+      {status: 201}
     );
   } catch (error: any) {
-    console.error('API Error:', error.message);
+    console.error("API Error:", error.message);
     return NextResponse.json(
-      { error: error.message ?? 'Internal Server Error' },
-      { status: 500 }
+      {error: error.message ?? "Internal Server Error"},
+      {status: 500}
+    );
+  }
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    const {searchParams} = new URL(req.url);
+    const id = searchParams.get("id");
+
+    if (!id) {
+      return NextResponse.json(
+        {error: "Workout ID is required"},
+        {status: 400}
+      );
+    }
+
+    const workout = await getWorkoutById(id);
+    return NextResponse.json(workout);
+  } catch (e: any) {
+    return NextResponse.json(
+      {error: e.message || "Failed to fetch workout"},
+      {status: 500}
     );
   }
 }
